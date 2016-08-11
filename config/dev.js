6823c590c20fab41653bb70f4b099f23486734c9
lowkey <lowkeystar@qq.com>
2016-08-11 23:53:37 +0800

    init

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,10 @@
 
+//开发配置
 module.exports = function(set, get, has){
 
     set('db.mongodb','mongodb://120.25.225.227/test');
 
-    set('db.serverUrl','http://120.25.225.227:9999/');
+    set('db.serverUrl','http://120.25.225.227:8080');
 
 
 }
--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 'use strict';
-
+//默认配置
 module.exports = function(set, get, has){
 
     //服务器监听端口
    set('web.port',3000);
+
    set('db.mongodb','mongodb://120.25.225.227/test');
+
    set('db.serverUrl','http://120.25.225.227:8080');
 
 
